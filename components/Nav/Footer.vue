ca8eefcbae40d03ed52759bf146a580019b71df4
Jeremiah Gage <jgage6@oreillyauto.com>
2024-10-14 21:38:43 -0500

    Finish SVGs; Bug fixes; COlor adjustments

--- components/Nav/Footer.vue
+++ components/Nav/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-    <footer class="py-4 border-t shadow-xl dark:bg-neutral-900 dark:text-neutral-200 dark:border-neutral-500">
+    <footer class="font-thin py-4 border-t shadow-xl text-neutral-800 dark:bg-neutral-900 dark:text-neutral-400 dark:border-neutral-500">
         <div class="container mx-auto text-center">
         <p>&copy; {{ new Date().getFullYear() }} Ozark Highland Shepherds. All rights reserved.</p>
         <!-- <nav class="flex justify-center gap-4 pb-4">
