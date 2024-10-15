b60f673b4a171617806fe8d42cb04522bd0ea7f9
Jeremiah Gage <jgage6@oreillyauto.com>
2024-10-14 21:48:45 -0500

    Finish SVGs; Bug fixes; COlor adjustments

--- globals/routes.ts
+++ globals/routes.ts
@@ -3,6 +3,7 @@ import breed from '~/assets/breed.json'
 
 export const getRoutes: () => Record<string, Route> = () => {
     return {
+        home: { path: '/', label: 'Home' },
         about: { path: '/faq', label: 'FAQ' },
         litters: { path: '/litters', label: 'Litters', disabled: breed.litters.length <= 1 },
         puppies: { path: '/puppies', label: 'New Puppies', disabled: breed.litters.length == 0 },
